426b33158182f849c3fc006db62065b4db825c80
Sc0utPsyX <sc0utpsyx@gmail.com>
2022-06-06 21:06:41 +0300

    add SocketTimeout

--- may-22-chat-master/chat-server/src/main/java/ru/gb/may_chat/server/Handler.java
+++ may-22-chat-master/chat-server/src/main/java/ru/gb/may_chat/server/Handler.java
@@ -65,7 +65,15 @@ public class Handler {
 
     private void authorize() {
         System.out.println("Authorizing");
-
+        Thread socketTimeout = new Thread(() -> {
+            try {
+                Thread.sleep(20000);
+                socket.close();
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        });
+        socketTimeout.start();
         try {
             while (!socket.isClosed()) {
                 String msg = in.readUTF();
@@ -93,6 +101,7 @@ public class Handler {
                         this.user = nickname;
                         send(AUTH_OK.getCommand() + REGEX + nickname);
                         server.addHandler(this);
+                        socketTimeout.interrupt();
                         break;
                     }
                 }
